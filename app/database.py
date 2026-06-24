3e3f9f0db463177f708e432d646e5baf7b6e9c13
genaroAE99 <81335084+genaroAE99@users.noreply.github.com>
2026-06-24 14:34:25 -0500

    cambio de la vista de citas vw_citas_PP_EPM_legalizacion por vw_callg_control_citas_con_historico

--- app/database.py
+++ app/database.py
@@ -66,7 +66,7 @@ class DatabaseManager:
             estado,
             nombre,
             fondo
-        FROM vw_citas_PP_EPM_legalizacion
+        FROM vw_callg_control_citas_con_historico
         WHERE nombre LIKE %s
         ORDER BY fecha DESC, hora_inicio DESC
         """
